4906b5fca26b4a53fdd38b9a87c46121cd4320ae
lolmaus (Andrey Mikhaylov) <lolmaus@gmail.com>
2017-09-18 18:30:03 +0300

    Preparing for release: default blueprint

---
+++ blueprints/ember-dialogs/index.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+module.exports = {
+  description : 'install ember-dialogs into a project',
+
+  normalizeEntityName () {},
+
+  // locals(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall(options) {
+  //   // Perform extra work here.
+  // }
+}
